e864a2317f505c6f25af97bb7eaf6ee704ecd6ac
fanfars <fanissd@gmail.com>
2022-05-07 19:52:23 +0400

    added mvvm

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,58 +1,50 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.dto.countFormat
+import ru.netology.nmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<PostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val post = Post(
-            id = 0L,
-            author = resources.getString(R.string.author),
-            content = resources.getString(R.string.content),
-            published = resources.getString(R.string.published),
-            likes = resources.getString(R.string.likesCount).toInt(),
-            shares = resources.getString(R.string.sharesCount).toInt(),
-            views = resources.getString(R.string.viewsCount).toInt()
-        )
-
-        binding.render(post)
-
-
-    }
-
-    private fun ActivityMainBinding.render(post: Post) {
-        author.text = post.author
-        content.text = post.content
-        published.text = post.published
-
-        likesButton?.setOnClickListener {
-            post.likedByMe = !post.likedByMe
-            likesButton.setImageResource(
-                getLikeIconResId(post.likedByMe)
-            )
-            likesCount.text = countFormat(if (post.likedByMe) (post.likes + 1) else (post.likes))
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                content.text = post.content
+                published.text = post.published
+                likesCount.text = countFormat(post.likes)
+                sharesCount.text = countFormat(post.shares)
+                viewsCount.text = countFormat(post.views)
+                likesButton.setImageResource(getLikeIconResId(post.likedByMe))
+            }
         }
 
-        shareButton?.setOnClickListener {
-            sharesCount.text = countFormat(post.shares++)
+        binding.likesButton.setOnClickListener {
+            viewModel.onLikeClicked()
         }
 
+        binding.shareButton.setOnClickListener {
+            viewModel.onShareClicked()
+        }
 
+        binding.viewsButton.setOnClickListener {
+            viewModel.onViewClicked()
+        }
     }
-
-    @DrawableRes
-    private fun getLikeIconResId(liked: Boolean) =
-        if (liked) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24dp
-
-
 }
+
+
+@DrawableRes
+private fun getLikeIconResId(liked: Boolean) =
+    if (liked) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24dp
+
+
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -2,6 +2,7 @@ package ru.netology.nmedia.dto
 
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import androidx.appcompat.app.AppCompatActivity
 
 data class Post(
     val id: Long,
@@ -17,22 +18,23 @@ data class Post(
 fun countFormat(count: Int): String {
     return when (count) {
         in 1..999 -> "${count.toString()}"
-        in 1000..1099 ->"${roundNoDecimal(count.toDouble()/1_000.0)}K"
-        in 1100..9_999 ->"${roundWithDecimal(count.toDouble()/1_000.0)}K"
-        in 10_000..999_999 ->"${roundNoDecimal(count.toDouble()/1_000.0)}K"
-        in 1_000_000..1_099_999 ->"${roundNoDecimal(count.toDouble()/1_000_000.0)}M"
-        in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(count.toDouble()/1_000_000.0)}M"
+        in 1000..1099 -> "${roundNoDecimal(count.toDouble() / 1_000.0)}K"
+        in 1100..9_999 -> "${roundWithDecimal(count.toDouble() / 1_000.0)}K"
+        in 10_000..999_999 -> "${roundNoDecimal(count.toDouble() / 1_000.0)}K"
+        in 1_000_000..1_099_999 -> "${roundNoDecimal(count.toDouble() / 1_000_000.0)}M"
+        in 1_100_000..Int.MAX_VALUE -> "${roundWithDecimal(count.toDouble() / 1_000_000.0)}M"
 
-        else-> count.toString()
+        else -> count.toString()
     }
 }
 
-fun roundWithDecimal(number: Double): Double? {
+fun roundWithDecimal(number: Double): Double {
     val df = DecimalFormat("#.#")
     df.roundingMode = RoundingMode.FLOOR
     return df.format(number).toDouble()
 }
-fun roundNoDecimal(number: Double): Int? {
+
+fun roundNoDecimal(number: Double): Int {
     val df = DecimalFormat("#")
     df.roundingMode = RoundingMode.FLOOR
     return df.format(number).toInt()
